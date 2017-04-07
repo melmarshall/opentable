aa8ec37d8e07c801eeb4260a461f13f14e1d010d
Mel Alrajhi <mgarcia@splunk.com>
2017-04-07 12:52:00 -0700

    compiling less

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,10 @@ var BUILD_DIR = path.resolve(__dirname, 'src/build');
 var APP_DIR = path.resolve(__dirname, 'src/app');
 
 module.exports = {
-    entry: APP_DIR + '/index.jsx',
+    entry: [
+        APP_DIR + '/index.jsx',
+        APP_DIR + '/less/styles.less'
+    ],
     output: {
         path: BUILD_DIR,
         filename: 'bundle.js'
@@ -16,7 +19,12 @@ module.exports = {
                 test: /\.jsx?/,
                 include: APP_DIR,
                 loader: 'babel-loader'
-            }
+            },
+            {
+              test: /\.less$/,
+              loader: "style-loader!css-loader!autoprefixer-loader!less-loader"
+            },
         ]
-    }
+    },
+    devtool: 'source-map'
 };
