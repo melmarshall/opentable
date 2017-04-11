523f3d8b9ceaa185e2b4470a04dcef894b35b1ba
Mel Alrajhi <mgarcia@splunk.com>
2017-04-10 20:49:55 -0700

    search bar component and styles

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -4,7 +4,37 @@ import {render} from 'react-dom';
 class App extends React.Component {
     render() {
         return (
-            <p>Hello React!</p>
+            <SearchBar />
+        );
+    }
+}
+
+class SearchBar extends React.Component {
+    constructor(){
+        super();
+
+        this.state = { value: ''};
+
+        this.onSearchSubmit = this.onSearchSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    onSearchSubmit(){
+        console.log(this.state.value);
+    }
+
+    handleChange(e){
+        this.setState({ value: e.target.value });
+    }
+
+    render (){
+        return (
+            <div className='landing-img'>
+                <div className='search-panel'>
+                    <input type='text' className='search-bar' onChange={ this.handleChange }/>
+                    <input type='submit' className='search-submit' onClick={ this.onSearchSubmit }/>
+                </div>
+            </div>
         );
     }
 }
