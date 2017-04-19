8473cca66766a97c46d3cf624d0165c643956cf0
Mel Alrajhi <mgarcia@splunk.com>
2017-04-18 20:00:41 -0700

    moving info to its own component

--- src/app/components/RestaurantCard.jsx
+++ src/app/components/RestaurantCard.jsx
@@ -1,23 +1,12 @@
 import React from 'react';
-import Price from './Price.jsx'
+import CardInfo from './CardInfo.jsx'
 
 class RestaurantCard extends React.Component {
     render(){
-        let address = `${this.props.restaurant.address}
-            ${this.props.restaurant.city}, ${this.props.restaurant.state} 
-            ${this.props.restaurant.postal_code}`;
-
         return (
             <div className='card'>
                 <img src={ this.props.restaurant.image_url }/>
-                <div className='info'>
-                    <div className='name'>{ this.props.restaurant.name }</div>
-                    <Price price={ this.props.restaurant.price } />
-                    <div className='reserve-link'>
-                        <a href={this.props.restaurant.reserve_url} className='reserve'>Reserve</a>
-                    </div>
-                    <div className='address'>{ address }</div>
-                </div>
+                <CardInfo restaurant={ this.props.restaurant } />
             </div>
         );
     }
