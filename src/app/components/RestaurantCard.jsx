9e530611e9abbb61d16f6eb6db36107eadde3df9
Mel Alrajhi <mgarcia@splunk.com>
2017-04-18 19:57:11 -0700

    adding all info

--- src/app/components/RestaurantCard.jsx
+++ src/app/components/RestaurantCard.jsx
@@ -3,12 +3,20 @@ import Price from './Price.jsx'
 
 class RestaurantCard extends React.Component {
     render(){
+        let address = `${this.props.restaurant.address}
+            ${this.props.restaurant.city}, ${this.props.restaurant.state} 
+            ${this.props.restaurant.postal_code}`;
+
         return (
             <div className='card'>
                 <img src={ this.props.restaurant.image_url }/>
                 <div className='info'>
                     <div className='name'>{ this.props.restaurant.name }</div>
                     <Price price={ this.props.restaurant.price } />
+                    <div className='reserve-link'>
+                        <a href={this.props.restaurant.reserve_url} className='reserve'>Reserve</a>
+                    </div>
+                    <div className='address'>{ address }</div>
                 </div>
             </div>
         );
